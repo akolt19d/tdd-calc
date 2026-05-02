7ecba0d930241e5f12810979639a5bc6f542f73a
Adam Kołt <a.kolt.19d@gmail.com>
2026-05-02 18:05:47 +0200

    feat: implement subtract method

--- app/src/test/java/tdd/calc/CalculatorTest.java
+++ app/src/test/java/tdd/calc/CalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
+    // ADD
     @Test
     void addsTwoPositiveNumbers() {
         Calculator calculator = new Calculator();
@@ -32,6 +32,37 @@ class CalculatorTest {
     @Test
     void addsDecimals() {
         Calculator calculator = new Calculator();
-        assertEquals(6.7, calculator.add(3.2, 3.5));
+        assertEquals(6.7, calculator.add(3.2, 3.5), 0.0001);
+    }
+
+    // SUBTRACT
+    @Test
+    void subtractsTwoPositiveNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(-2.0, calculator.subtract(2.0, 4.0));
+    }
+
+    @Test
+    void subtractsTwoNegativeNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(-53.0, calculator.subtract(-60.0, -7.0));
+    }
+
+    @Test
+    void subtractsPositiveAndNegative() {
+        Calculator calculator = new Calculator();
+        assertEquals(6.0, calculator.subtract(2.0, -4.0));
+    }
+
+    @Test
+    void subtractsZero() {
+        Calculator calculator = new Calculator();
+        assertEquals(5.0, calculator.subtract(5.0, 0.0));
+    }
+
+    @Test
+    void subtractsDecimals() {
+        Calculator calculator = new Calculator();
+        assertEquals(6.7, calculator.subtract(9.9, 3.2), 0.0001);
     }
 }
