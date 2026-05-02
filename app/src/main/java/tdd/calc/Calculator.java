60c6164d25e2ab5c4c73bb6b00a15bdf838b2806
Adam Kołt <a.kolt.19d@gmail.com>
2026-05-02 18:32:45 +0200

    refactor: make calculator a static class

--- app/src/main/java/tdd/calc/Calculator.java
+++ app/src/main/java/tdd/calc/Calculator.java
@@ -2,19 +2,22 @@ package tdd.calc;
 
 public class Calculator {
 
-    public double add(double a, double b) {
+    private Calculator() {
+    }
+
+    public static double add(double a, double b) {
         return a + b;
     }
 
-    public double subtract(double a, double b) {
+    public static double subtract(double a, double b) {
         return a - b;
     }
 
-    public double multiply(double a, double b) {
+    public static double multiply(double a, double b) {
         return a * b;
     }
 
-    public double divide(double a, double b) {
+    public static double divide(double a, double b) {
         if (b == 0.0)
             throw new IllegalArgumentException("Division by zero");
 
--- app/src/test/java/tdd/calc/CalculatorTest.java
+++ app/src/test/java/tdd/calc/CalculatorTest.java
@@ -7,130 +7,109 @@ class CalculatorTest {
     // ADD
     @Test
     void addsTwoPositiveNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(6.0, calculator.add(2.0, 4.0));
+        assertEquals(6.0, Calculator.add(2.0, 4.0));
     }
 
     @Test
     void addsTwoNegativeNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(-67.0, calculator.add(-60.0, -7.0));
+        assertEquals(-67.0, Calculator.add(-60.0, -7.0));
     }
 
     @Test
     void addsPositiveAndNegative() {
-        Calculator calculator = new Calculator();
-        assertEquals(-2.0, calculator.add(2.0, -4.0));
+        assertEquals(-2.0, Calculator.add(2.0, -4.0));
     }
 
     @Test
     void addsZero() {
-        Calculator calculator = new Calculator();
-        assertEquals(0.0, calculator.add(0.0, 0.0));
+        assertEquals(0.0, Calculator.add(0.0, 0.0));
     }
 
     @Test
     void addsDecimals() {
-        Calculator calculator = new Calculator();
-        assertEquals(6.7, calculator.add(3.2, 3.5), 0.0001);
+        assertEquals(6.7, Calculator.add(3.2, 3.5), 0.0001);
     }
 
     // SUBTRACT
     @Test
     void subtractsTwoPositiveNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(-2.0, calculator.subtract(2.0, 4.0));
+        assertEquals(-2.0, Calculator.subtract(2.0, 4.0));
     }
 
     @Test
     void subtractsTwoNegativeNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(-53.0, calculator.subtract(-60.0, -7.0));
+        assertEquals(-53.0, Calculator.subtract(-60.0, -7.0));
     }
 
     @Test
     void subtractsPositiveAndNegative() {
-        Calculator calculator = new Calculator();
-        assertEquals(6.0, calculator.subtract(2.0, -4.0));
+        assertEquals(6.0, Calculator.subtract(2.0, -4.0));
     }
 
     @Test
     void subtractsZero() {
-        Calculator calculator = new Calculator();
-        assertEquals(5.0, calculator.subtract(5.0, 0.0));
+        assertEquals(5.0, Calculator.subtract(5.0, 0.0));
     }
 
     @Test
     void subtractsDecimals() {
-        Calculator calculator = new Calculator();
-        assertEquals(6.7, calculator.subtract(9.9, 3.2), 0.0001);
+        assertEquals(6.7, Calculator.subtract(9.9, 3.2), 0.0001);
     }
 
     // MULTIPLY
     @Test
     void multipliesTwoPositiveNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(8.0, calculator.multiply(2.0, 4.0));
+        assertEquals(8.0, Calculator.multiply(2.0, 4.0));
     }
 
     @Test
     void multipliesTwoNegativeNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(6.0, calculator.multiply(-2.0, -3.0));
+        assertEquals(6.0, Calculator.multiply(-2.0, -3.0));
     }
 
     @Test
     void multipliesPositiveAndNegative() {
-        Calculator calculator = new Calculator();
-        assertEquals(-6.0, calculator.multiply(2.0, -3.0));
+        assertEquals(-6.0, Calculator.multiply(2.0, -3.0));
     }
 
     @Test
     void multipliesByZero() {
-        Calculator calculator = new Calculator();
-        assertEquals(0.0, calculator.multiply(5.0, 0.0));
+        assertEquals(0.0, Calculator.multiply(5.0, 0.0));
     }
 
     @Test
     void multipliesDecimals() {
-        Calculator calculator = new Calculator();
-        assertEquals(3.75, calculator.multiply(2.5, 1.5), 0.0001);
+        assertEquals(3.75, Calculator.multiply(2.5, 1.5), 0.0001);
     }
 
     // DIVIDE
     @Test
     void dividesTwoPositiveNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(2.0, calculator.divide(4.0, 2.0));
+        assertEquals(2.0, Calculator.divide(4.0, 2.0));
     }
 
     @Test
     void dividesTwoNegativeNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(2.0, calculator.divide(-4.0, -2.0));
+        assertEquals(2.0, Calculator.divide(-4.0, -2.0));
     }
 
     @Test
     void dividesPositiveAndNegative() {
-        Calculator calculator = new Calculator();
-        assertEquals(-2.0, calculator.divide(4.0, -2.0));
+        assertEquals(-2.0, Calculator.divide(4.0, -2.0));
     }
 
     @Test
     void dividesDecimals() {
-        Calculator calculator = new Calculator();
-        assertEquals(1.5, calculator.divide(7.5, 5.0), 0.0001);
+        assertEquals(1.5, Calculator.divide(7.5, 5.0), 0.0001);
     }
 
     @Test
     void canDivideZero() {
-        Calculator calculator = new Calculator();
-        assertEquals(0.0, calculator.divide(0.0, 10.0));
+        assertEquals(0.0, Calculator.divide(0.0, 10.0));
     }
 
     @Test
     void cannotDivideByZero() {
-        Calculator calculator = new Calculator();
-        assertThrows(IllegalArgumentException.class, () -> calculator.divide(10.0, 0.0));
+        assertThrows(IllegalArgumentException.class, () -> Calculator.divide(10.0, 0.0));
     }
 }
